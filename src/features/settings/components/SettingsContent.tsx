903c6a246b680fb2b43c1c3a6a089c77156c5e87
omeresmr <bxnger36@gmail.com>
2026-03-01 03:35:56 +0100

    implement SettingsHeader & fix layouting

--- src/features/settings/components/SettingsContent.tsx
+++ src/features/settings/components/SettingsContent.tsx
@@ -3,6 +3,7 @@ import Description from '../../../shared/ui/description/Description';
 import Icon from '../../../shared/ui/icon/Icon';
 import SettingsCard from './SettingsCard';
 import ToggleButton from '../../../shared/ui/buttons/ToggleButton';
+import SettingsHeader from './SettingsHeader';
 
 export default function SettingsContent() {
   return (
@@ -10,17 +11,16 @@ export default function SettingsContent() {
       <p className="text-2xl font-bold">Settings</p>
       <Description text="Customize your Pomodoro experience" />
       <SettingsCard>
-        <div className="settings-heading">
-          <Icon className="bg-brand-primary/15 text-brand-secondary p-2">
-            <Clock className="w-4 h-4" />
-          </Icon>
-          <div className="settings-heading-text">
-            <p className="text-base font-bold">Timer</p>
-            <p className="text-sm text-muted-foreground">
-              Configure your pomodoro timer
-            </p>
-          </div>
-        </div>
+        <SettingsHeader
+          icon={
+            <Icon className="bg-brand-primary/15 text-brand-secondary p-2">
+              <Clock className="w-4 h-4" />
+            </Icon>
+          }
+          title="Timer"
+          description="Configure your pomodoro timer"
+        />
+
         <div className="setting-con border-b border-border py-4">
           <div className="setting-text-con">
             <p className="text-sm font-semibold">Focus Session</p>
@@ -67,18 +67,15 @@ export default function SettingsContent() {
         </div>
       </SettingsCard>
       <SettingsCard>
-        <div className="settings-heading">
-          <Icon className="p-2 bg-violet-600/15 text-violet-600">
-            <Zap className="w-4 h-4" />
-          </Icon>
-
-          <div className="settings-heading-text">
-            <p className="text-base font-bold">Automation</p>
-            <p className="text-sm text-muted-foreground">
-              Auto-start preferences
-            </p>
-          </div>
-        </div>
+        <SettingsHeader
+          icon={
+            <Icon className="p-2 bg-violet-600/15 text-violet-600">
+              <Zap className="w-4 h-4" />
+            </Icon>
+          }
+          title="Automation"
+          description="Auto-start preferences"
+        />
         <div className="setting-con border-b border-border py-4">
           <div className="setting-text-con">
             <p className="text-sm font-semibold">Auto-start breaks</p>
@@ -92,7 +89,7 @@ export default function SettingsContent() {
         </div>
         <div className="setting-con pt-4">
           <div className="setting-text-con">
-            <p className="text-sm font-semibold">Auto-start breaks</p>
+            <p className="text-sm font-semibold">Auto-start pomodoros</p>
             <p className="text-sm text-muted-foreground">
               Start pomodoros automatically
             </p>
